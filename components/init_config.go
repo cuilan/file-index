e390b00a8fd54fb88b921572dd56ffca912fa029
zhangyan <17746598041@163.com>
2021-11-13 02:06:12 +0800

    feature(folder):递归读取目录

--- components/init_config.go
+++ components/init_config.go
@@ -2,13 +2,17 @@ package components
 
 import (
 	"fmt"
+
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/logs"
+	"github.com/cuilan/go-common/load"
+	"gorm.io/gorm"
 )
 
 var (
 	AppName  string
 	ScanPath string
+	Db       *gorm.DB
 )
 
 func InitConfiger() {
@@ -16,6 +20,10 @@ func InitConfiger() {
 	logs.Info("init app config start...")
 	AppName = beego.AppConfig.String("app::app_name")
 	ScanPath = beego.AppConfig.String("app::scan_path")
+	// 加载日志配置
+	load.LoadLoggerConfig("./conf/log.conf")
+	// 读取配置，并初始化数据源
+	Db = load.LoadMysqlConfig("./conf/mysql.conf")
 	logs.Info("init app config end...")
 }
 
--- service/index.go
+++ service/index.go
@@ -2,48 +2,104 @@ package service
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
+	"path"
+	"strings"
+
 	"github.com/astaxie/beego/logs"
 	"github.com/cuilan/file-index/components"
-	"os"
-	"path/filepath"
-	"strings"
+	"github.com/cuilan/file-index/dao"
+)
+
+var (
+	Separator string = "/"
 )
 
 func Index() {
 	logs.Info("Application [%s] started...", components.AppName)
 	logs.Info("start index file, path: %s", components.ScanPath)
 
-	err := filepath.Walk(components.ScanPath, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
+	// 配置中的目录包含 "/" 说明不是根目录，需要特殊处理
+	parentId := baseDir(components.ScanPath)
+	logs.Info("config scan_path: %s, parentId: %d", components.ScanPath, parentId)
 
-		if info.IsDir() {
-			// 目录
-			processFolder(path)
-		} else {
-			// 文件
-			processFile(path, info)
-		}
-		return nil
-	})
-	if err != nil {
-		logs.Error("read file fail, path: %s, error: %v", components.ScanPath, err.Error())
-		panic(err)
-	}
+	// 处理配置目录
+	readDir(parentId, components.ScanPath)
 }
 
-// processFolder 处理目录
-func processFolder(path string) {
-	//fmt.Println(path)
-	if strings.Contains(path, "/") {
-		split := strings.Split(path, "/")
-		folderName := split[len(split)-1]
-		fmt.Println(folderName)
-		fmt.Println(path)
-		// TODO save
-	} else {
+// baseDir 处理基础目录
+func baseDir(dir string) (parentId int64) {
+	if strings.Contains(dir, Separator) {
+		logs.Info("app.conf scan_path is not root path, start process...")
+		parentId = 0
+		var dirName, dirPath, dirType, desc string
+		dirs := strings.Split(dir, Separator)
+		for i := range dirs {
+			dirName = dirs[i]
+			existDir := dao.QueryFolderExist(parentId, dirName)
+			// id=0，则目录不存在
+			if existDir.Id != int64(0) {
+				parentId = existDir.Id
+				continue
+			}
+			// 根目录
+			if parentId == 0 {
+				dirPath = dirName
+				dirType = "盘符"
+				desc = "根目录"
 
+				logs.Info("baseDir create root dir: %s", dirName)
+				parentId = dao.AddFolder(parentId, dirName, dirPath, dirType, desc)
+				dirType = ""
+				desc = ""
+			} else {
+				// 如果当前目录名称不是根目录，则需要拼接绝对路径
+				dirPath = path.Join(dirPath, dirName)
+				logs.Info("baseDir create dir: %s", dirName)
+				parentId = dao.AddFolder(parentId, dirName, dirPath, dirType, desc)
+			}
+		}
+	}
+	return
+}
+
+func readDir(parentId int64, dir string) {
+	logs.Info("read dir: %s, parentId: %d", dir, parentId)
+	infos, err := ioutil.ReadDir(dir)
+	if err != nil {
+		logs.Error("read dir: %s, error: %v", dir, err.Error())
+		return
+	}
+	for i := range infos {
+		info := infos[i]
+		// 处理目录
+		if info.IsDir() {
+			var dirName, dirPath, dirType, desc string
+			dirName = info.Name()
+
+			// 忽略隐藏目录
+			if dirName[0:1] == "." {
+				continue
+			}
+
+			// 拼接绝对路径
+			dirPath = path.Join(dir, dirName)
+			existDir := dao.QueryFolderExist(parentId, dirName)
+			var currentId int64
+			if existDir.Id == 0 {
+				// id=0，则目录不存在
+				logs.Info("readDir create dir: %s", dirName)
+				currentId = dao.AddFolder(parentId, dirName, dirPath, dirType, desc)
+			} else {
+				// id!=0，则目录已存在
+				currentId = existDir.Id
+			}
+
+			readDir(currentId, dirPath)
+		} else {
+			// TODO 目录已存在，处理文件
+		}
 	}
 }
 
