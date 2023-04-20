049b55f334457015418c6aa9dce5b5ae18888ddb
zhangyan <419475937@qq.com>
2023-04-20 23:40:13 +0800

    feat(win):修改Windows下编译问题

--- build.bat
+++ build.bat
@@ -4,19 +4,17 @@ REM @auther zhang.yan
 REM @date 2021-10-20
 TITLE build
 
-go build -o main.exe .
+go build -o file-index.exe .
 
 rd /q /s dist
 mkdir dist
 mkdir dist\conf\
 mkdir dist\logs\
+mkdir dist\db\
 
-copy main.exe .\dist\
+copy file-index.exe .\dist\
 copy .\bat\start.bat .\dist\
 copy .\bat\clear.bat .\dist\
 copy .\conf\ .\dist\conf\
-
-cd dist
-ren main.exe FileIndex.exe
 
 REM PAUSE
